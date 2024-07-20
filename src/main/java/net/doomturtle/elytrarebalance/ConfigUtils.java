2cc38f53d3973c6b565dfb2f5b7e43b4c38de27d
Max Wawrina <maximilian.wawrina@tugraz.at>
2024-07-20 15:42:56 +0200

    cleaned up imports

--- src/main/java/net/doomturtle/elytrarebalance/ConfigUtils.java
+++ src/main/java/net/doomturtle/elytrarebalance/ConfigUtils.java
@@ -1,12 +1,8 @@
 package net.doomturtle.elytrarebalance;
-import java.io.BufferedReader;
+
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-
-import java.io.*;
 import java.nio.file.*;
 import java.util.List;
 
